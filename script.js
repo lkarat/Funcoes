3b3164aec4671012ce7b224a6aa4ec4ee1b24c26
Luiz Karat <luiz.karat.silva@gmail.com>
2024-04-24 08:57:47 -0300

    exercicio1

---
+++ script.js
@@ -0,0 +1,20 @@
+//function imprimirNome(pessoa) {
+//    console.log("Olá", pessoa);  
+//}
+//imprimirNome("Ricardinho")
+//imprimirNome("Britany")
+//imprimirNome("Joshua")
+//imprimirNome("Stephany")
+
+// Fuções não nomeada
+
+/*let imprimirNome= function (nome) {
+let nominho=nome
+console.log(nominho);
+
+}
+
+imprimirNome("Gustavo Gustamante")
+*/
+
+
