b427ca0a5de603e1d257a55c03f293ae63dc0e3a
Luiz Karat <luiz.karat.silva@gmail.com>
2024-04-25 08:20:37 -0300

    exercicio2

--- script.js
+++ script.js
@@ -17,3 +17,16 @@ console.log(nominho);
 imprimirNome("Gustavo Gustamante")
 */
 
+// Prática 2
+
+//function algebricar(numero) {
+  // let a=numero%2===0
+   //let b=numero+10
+   //let c=numero*numero
+   //return `o número ${numero} é par? ${a}. Somado com 1O é igual a ${b} e multiplicado por ele mesmo é  ${c}`
+    
+//}
+
+//console.log(algebricar(55))
+
+
